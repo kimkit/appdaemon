c7de75c1abb2094bf8ee0f7cce827fb78a4d25a1
wuwenbin <wenbin.wu@foxmail.com>
2019-07-25 15:21:18 +0800

    Change filter rule

--- pkg/cmdsvr/tasklist.go
+++ pkg/cmdsvr/tasklist.go
@@ -26,7 +26,7 @@ func (cmd *taskListCommand) S1Handler(_cmd *redsvr.Command, args []string, conn 
 		name, _ := k.(string)
 		job, _ := v.(*jobctl.Job)
 		if job != nil {
-			if strings.HasPrefix(name, GetTaskKeyPrefix()) {
+			if len(args) == 0 {
 				list = append(list, []string{
 					common.Time2str(common.GetMapValueInt(&job.Map, "last")),
 					"|",
@@ -36,20 +36,25 @@ func (cmd *taskListCommand) S1Handler(_cmd *redsvr.Command, args []string, conn 
 					"|",
 					strings.Join(common.GetMapValueStringArr(&job.Map, "args"), " "),
 				})
+				return true
+			}
+			for _, filter := range args {
+				if strings.HasPrefix(name, GetTaskKey(filter)+"_") || name == GetTaskKey(filter) {
+					list = append(list, []string{
+						common.Time2str(common.GetMapValueInt(&job.Map, "last")),
+						"|",
+						common.GetMapValueString(&job.Map, "name"),
+						"|",
+						common.GetMapValueString(&job.Map, "rule"),
+						"|",
+						strings.Join(common.GetMapValueStringArr(&job.Map, "args"), " "),
+					})
+				}
 			}
 		}
 		return true
 	})
 	_list := common.BuildTable(list)
-	for _, filter := range args {
-		var __list []string
-		for _, line := range _list {
-			if strings.Contains(line, filter) {
-				__list = append(__list, line)
-			}
-		}
-		_list = __list
-	}
 	sort.Strings(_list)
 	redsvr.WriteArray(conn, _list)
 	return nil
