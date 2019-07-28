ad719a3efa44fbd5fc38bb7fc158bfaea5dbebeb
wuwenbin <wenbin.wu@foxmail.com>
2019-07-28 09:15:09 +0800

    Update config

--- scripts/getipinfo.php
+++ scripts/getipinfo.php
@@ -2,8 +2,5 @@
 
 date_default_timezone_set('Asia/Shanghai');
 
-printf(
-    "[%s] %s\n",
-    date('Y-m-d H:i:s'),
-    trim(file_get_contents('http://myip.ipip.net'))
-);
+printf("[%s] %s\n", date('Y-m-d H:i:s'), trim(file_get_contents('http://myip.ipip.net')));
+
--- scripts/getipinfoloop.php
+++ scripts/getipinfoloop.php
@@ -6,11 +6,7 @@ $signalhandler = function ($signo) {
     if ($signo == SIGINT) {
         printf("\n");
     }
-    printf(
-        "[%s] recv signal: %d\n",
-        date('Y-m-d H:i:s'),
-        $signo
-    );
+    printf("[%s] recv signal: %d\n", date('Y-m-d H:i:s'), $signo);
     exit(0);
 };
 
@@ -22,18 +18,15 @@ $lasttime = 0;
 
 while (true) {
     pcntl_signal_dispatch();
+
     $now = time();
     if ($now >= $lasttime + 10) {
         $lasttime = $now;
-        printf(
-            "[%s] %s\n",
-            date('Y-m-d H:i:s'),
-            trim(file_get_contents('http://myip.ipip.net'))
-        );
+        printf("[%s] %s\n", date('Y-m-d H:i:s'), trim(file_get_contents('http://myip.ipip.net')));
     }
+
     usleep(100000);
-    $statusfile = getenv("TASK_STATUS_FILE");
-    if ($statusfile) {
+    if ($statusfile = getenv("TASK_STATUS_FILE")) {
         file_put_contents($statusfile, $now);
     }
 }
